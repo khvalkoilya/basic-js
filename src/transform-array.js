2c8801041a24e5fb8035d118da2cefad70c92c95
khvalkoilya <khvalko.ilya@gmail.com>
2020-02-23 21:21:04 +0300

    feat: add 100% solution to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw new Error();
+    let newArr=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]=="--double-next") {
+            if(i==arr.length-1) i++;
+            else newArr.push(arr[i+1]);
+            continue;
+        }
+        else if(arr[i]=="--double-prev"){
+            if(i==0);
+            else newArr.push(arr[i-1]);
+            continue;
+        }
+        else if(arr[i]=="--discard-next"){
+            if(i==arr.length-1) i++;
+            else i++;
+            continue;
+        }
+        else if(arr[i]=="--discard-prev"){
+            if(i==0);
+            else newArr.pop();
+            continue;
+        }
+        newArr.push(arr[i]);
+    }
+    return newArr;
 };
